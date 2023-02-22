9dbef3d561a0f412dcaf2dc59ce153509704113c
ElisaVelasquez <101837234+ElisaVelasquez@users.noreply.github.com>
2023-02-22 07:19:09 -0700

    Practica5
    Se agrego lo del menu de antojitos

--- app/src/main/java/mx/edu/itson/practica4/MenuActivity.kt
+++ app/src/main/java/mx/edu/itson/practica4/MenuActivity.kt
@@ -2,10 +2,18 @@ package mx.edu.itson.practica4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        var btnAntojitos: Button = findViewById(R.id.btnAntojitos) as Button
+
+        btnAntojitos.setOnClickListener {
+            var intent: Intent = Intent(this,ProductosActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
